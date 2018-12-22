eb01180bad16c4110ad9ecdf273f747118c049cd
Blanc293 <xiaobaiaha@163.com>
2018-12-22 14:39:49 +0800

    提交代码

---
+++ src/Game/SharePic.ts
@@ -0,0 +1,51 @@
+class SharePic extends eui.Component implements eui.UIComponent{
+	public constructor(func:Function,data) {
+		super();
+		this.addEventListener(eui.UIEvent.COMPLETE,this.onComplete,this);
+		this.skinName = "resource/skins/SharePicSkins.exml";
+        this.action = func;
+        if(data.stageObj.canHarvest == "true"){
+            this.share_label.text = "我的"+data.treeName+"结果了!"
+        }
+        else{
+            this.share_label.text = "我的"+data.treeName+data.stageObj.name+"了!"
+        }
+	}
+
+    private action:Function;
+	private img_tree:eui.Image;
+    private share_label:eui.Label;
+    private btn_share:eui.Image;
+    private btn_cancel:eui.Image;
+
+    protected childrenCreated():void{
+		super.childrenCreated();
+        var renderTexture:egret.RenderTexture = new egret.RenderTexture();
+			renderTexture.drawToTexture(SceneManager.sceneManager.mainScene.tree);
+        this.img_tree.texture = renderTexture;
+    }
+
+
+    private onComplete():void{
+        SceneManager.sceneManager.mainScene.enabled = false;
+        this.btn_share.addEventListener(egret.TouchEvent.TOUCH_TAP,this.goshare,this);
+        this.btn_cancel.addEventListener(egret.TouchEvent.TOUCH_TAP,this.remove,this)
+    }
+
+    private remove(){
+        SceneManager.sceneManager.mainScene.enabled = true;
+        if(this.parent){
+            this.parent.removeChild(this);
+        }
+    }
+
+    private goshare(){
+        SceneManager.sceneManager.mainScene.enabled = true;
+        if(this.parent){
+            this.parent.removeChild(this);
+        }
+        this.action();
+    }
+}
+
+
--- src/Http/HttpRequest.ts
+++ src/Http/HttpRequest.ts
@@ -1,15 +1,20 @@
 class HttpRequest {
     public static map: { [key: string]: egret.Texture } = {};//创建一个map,用于存放用户头像，减少加载
     //加载网络图片
-    public static imageloader(url, image,user?:string) {
+    public static imageloader(url, image, user?: string, success?: Function, _this?: Object) {
         try {
-            if(user && this.map[user]){
+            if (user && this.map[user]) {
                 image.texture = this.map[user];
+                if (_this) {
+                    success.call(_this)
+                } else {
+                    success
+                }
                 return;
             }
             let imgLoader = new egret.ImageLoader();
             imgLoader.crossOrigin = "anonymous";// 跨域请求
-            if(url == Config.picurl || url == Config.picurl+"undefined"){
+            if (url == Config.picurl || url == Config.picurl + "undefined") {
                 return;
             }
             imgLoader.load(url);// 去除链接中的转义字符‘\’        
@@ -18,17 +23,22 @@ class HttpRequest {
                     let texture = new egret.Texture();
                     texture.bitmapData = evt.currentTarget.data;
                     image.texture = texture;
-                    if(user && !this.map[user]){//保存用户初次头像
+                    if (_this) {
+                        success.call(_this)
+                    } else {
+                        success
+                    }
+                    if (user && !this.map[user]) {//保存用户初次头像
                         this.map[user] = texture;
                     }
                 }
             }, this);
-        } catch (e) { console.log("图片加载错误",e) }
+        } catch (e) { console.log("图片加载错误", e) }
     }
 
-    public static imageByUrl(url,image:eui.Image){
-         RES.getResByUrl(url,function(data){
+    public static imageByUrl(url, image: eui.Image) {
+        RES.getResByUrl(url, function (data) {
             image.texture = data;
-        },this,RES.ResourceItem.TYPE_IMAGE);
+        }, this, RES.ResourceItem.TYPE_IMAGE);
     }
 }
